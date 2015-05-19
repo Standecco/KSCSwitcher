e15e7efcc86ffbb6e2b708dd514a1a0465981b5a
NathanKell <nathankell@gmail.com>
2015-05-19 15:39:57 -0400

    Streamline and NRE-proof the city/decal getters

--- KSCSwitcher/KSCSwitcher.cs
+++ KSCSwitcher/KSCSwitcher.cs
@@ -192,18 +192,12 @@ namespace regexKSP {
 
 		public static PQSCity findKSC(CelestialBody home) {
             if(home != null) {
-                if(home.gameObject != null && home.gameObject.transform != null) {
-                    Transform t = home.pqsController.transform.Find("KSC");
-                    if(t != null) {
-                        PQSCity KSC = (PQSCity)t.GetComponent(typeof(PQSCity));
-                        return KSC;
-                    }
-                }
-
                 if(home.pqsController != null && home.pqsController.transform != null) {
 		            Transform t = home.pqsController.transform.Find("KSC");
-		            PQSCity KSC = (PQSCity)t.GetComponent(typeof(PQSCity));
-		            return KSC;
+                    if(t != null) {
+                        PQSCity KSC = (PQSCity)t.GetComponent(typeof(PQSCity));
+                        if(KSC != null) { return KSC; }
+                    }
                 }
             }
 
@@ -219,18 +213,12 @@ namespace regexKSP {
 
 		public static PQSMod_MapDecalTangent findKSCMapDecal(CelestialBody home) {
             if(home != null) {
-                if(home.gameObject != null && home.gameObject.transform != null) {
-                    Transform t = home.gameObject.transform.Find("KSC");
-                    if(t != null) {
-                        PQSMod_MapDecalTangent decal = (PQSMod_MapDecalTangent)t.GetComponent(typeof(PQSMod_MapDecalTangent));
-                        return decal;
-                    }
-                }
-
                 if(home.pqsController != null && home.pqsController.transform != null) {
                     Transform t = home.pqsController.transform.Find("KSC");
-                    PQSMod_MapDecalTangent decal = (PQSMod_MapDecalTangent)t.GetComponent(typeof(PQSMod_MapDecalTangent));
-                    return decal;
+                    if(t != null) {
+                        PQSMod_MapDecalTangent decal = (PQSMod_MapDecalTangent)t.GetComponent(typeof(PQSMod_MapDecalTangent));
+                        if(decal != null) { return decal; }
+                    }
                 }
             }
 
